2003496899be6f42cba20f5d91cbc540c00c772d
dabi <dabi@DABI-NOC>
2016-04-29 19:14:56 +0200

    Add project files.

---
+++ Thai_letters_1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Thai_letters_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.debugText = new System.Windows.Forms.TextBox();
+            this.scoreBoard = new System.Windows.Forms.Label();
+            this.lblThaiSymbol = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(39, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(115, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 1;
+            this.button2.TabStop = false;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(186, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 2;
+            this.button3.TabStop = false;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // debugText
+            // 
+            this.debugText.Location = new System.Drawing.Point(41, 149);
+            this.debugText.Name = "debugText";
+            this.debugText.Size = new System.Drawing.Size(194, 20);
+            this.debugText.TabIndex = 4;
+            // 
+            // scoreBoard
+            // 
+            this.scoreBoard.AutoSize = true;
+            this.scoreBoard.Font = new System.Drawing.Font("Mistral", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreBoard.Location = new System.Drawing.Point(198, 249);
+            this.scoreBoard.Name = "scoreBoard";
+            this.scoreBoard.Size = new System.Drawing.Size(38, 26);
+            this.scoreBoard.TabIndex = 0;
+            this.scoreBoard.Text = "0/0";
+            // 
+            // lblThaiSymbol
+            // 
+            this.lblThaiSymbol.AutoSize = true;
+            this.lblThaiSymbol.Font = new System.Drawing.Font("Microsoft Sans Serif", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThaiSymbol.Location = new System.Drawing.Point(75, -5);
+            this.lblThaiSymbol.Name = "lblThaiSymbol";
+            this.lblThaiSymbol.Size = new System.Drawing.Size(124, 135);
+            this.lblThaiSymbol.TabIndex = 0;
+            this.lblThaiSymbol.Text = "ด";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 295);
+            this.Controls.Add(this.lblThaiSymbol);
+            this.Controls.Add(this.scoreBoard);
+            this.Controls.Add(this.debugText);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Draco\'s Thai App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox debugText;
+        private System.Windows.Forms.Label scoreBoard;
+        private System.Windows.Forms.Label lblThaiSymbol;
+    }
+}
+
---
+++ Thai_letters_1/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+
+namespace Thai_letters_1
+{
+    public partial class Form1 : Form
+    {
+        int randomRijNr, correctButton, score = 0, tries = 0;
+        string[,] thaiTable;
+        SoundPlayer splayerCorrect = new SoundPlayer(Properties.Resources.correct);
+        SoundPlayer splayerWrong = new SoundPlayer(Properties.Resources.wrong);
+        //SoundPlayer spWrong = new SoundPlayer(@"c:\temp\wrong.wav");
+        public Form1()
+        {
+            FillArrays();
+            FillArrayWithXML();
+            InitializeComponent();
+            Randomizer();
+        }
+
+        private void FillArrayWithXML() //not finished yet
+        {
+            //var path = @"C:\Users\dabi\Dropbox\C-sharp\Projects\Thai_letters_1\Thai_letters_1\thaiTable.xml";
+            //var thaiTableXML=XDocument.Load(path).Root.Elements().Select(y => y.Elements().ToDictionary(x => x.Name, x => x.Value)).ToArray();
+            var thaiTableXML = XDocument.Load(Path.GetFullPath("thaiTable.xml")).Root.Elements().Select(y => y.Elements().ToDictionary(x => x.Name, x => x.Value)).ToArray();
+        }
+
+        private void Randomizer()
+        {
+            Random rnd = new Random();
+            int previousRandomRijNr = randomRijNr;
+            while (previousRandomRijNr == randomRijNr)
+            {
+                randomRijNr = rnd.Next(0, thaiTable.GetLength(0)); //Randomizes row number, rerandomize when number is the same, (1) for column number
+            }
+            lblThaiSymbol.Text = thaiTable[randomRijNr, 0];
+
+            string correctSound = thaiTable[randomRijNr, 1], wrongSound1 = "", wrongSound2 = "", sType = thaiTable[randomRijNr, 4];
+            while (correctSound == wrongSound1 || correctSound == wrongSound2 || wrongSound1 == wrongSound2) //making sure not same 'sounds' are getting used and for now only Consonants will be used
+            {
+                wrongSound1 = thaiTable[rnd.Next(0, thaiTable.GetLength(0)), 1];
+                wrongSound2 = thaiTable[rnd.Next(0, thaiTable.GetLength(0)), 1];
+            }
+
+            correctButton = rnd.Next(3); //vul de buttons in
+            switch (correctButton)
+            {
+                case 0:
+                    button1.Text = correctSound;
+                    button2.Text = wrongSound1;
+                    button3.Text = wrongSound2;
+                    break;
+                case 1:
+                    button1.Text = wrongSound1;
+                    button2.Text = correctSound;
+                    button3.Text = wrongSound2;
+                    break;
+                case 2:
+                    button1.Text = wrongSound2;
+                    button2.Text = wrongSound1;
+                    button3.Text = correctSound;
+                    break;
+                default:
+                    debugText.Text = "Error in correctButton";
+                    throw new Exception();
+            }
+        }
+
+
+        private async void Scores(bool correct)
+        {
+            if (correct)
+            {
+                debugText.BackColor = Color.Green;
+                debugText.Text = "Correct!";
+                score++;
+                tries++;
+                scoreBoard.Text = string.Format("{0}/{1}", score, tries);
+                lblThaiSymbol.Focus(); //drops focus from buttons
+                splayerCorrect.Play();
+                await Task.Delay(1000);
+            }
+            else
+            {
+                debugText.BackColor = Color.Red;
+                debugText.Text = "Incorrect!";
+                tries++;
+                scoreBoard.Text = string.Format("{0}/{1}", score, tries);
+                lblThaiSymbol.Focus(); //drops focus from buttons
+                splayerWrong.Play();
+                switch (correctButton)
+                {
+                    case 0:
+                        button1.BackColor = Color.Green;
+                        await Task.Delay(1000);
+                        button1.BackColor = default(Color);
+                        break;
+                    case 1:
+                        button2.BackColor = Color.Green;
+                        await Task.Delay(1000);
+                        button2.BackColor = default(Color);
+                        break;
+                    case 2:
+                        button3.BackColor = Color.Green;
+                        await Task.Delay(1000);
+                        button3.BackColor = default(Color);
+                        break;
+                    default:
+                        debugText.Text = "Error in buttonscorecolors";
+                        throw new Exception();
+                }
+
+            }
+            debugText.BackColor = default(Color);
+            debugText.Text = "";
+            Randomizer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (correctButton == 0)
+            {
+                Scores(true);
+            }
+            else
+            {
+                Scores(false);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (correctButton == 1)
+            {
+                Scores(true);
+            }
+            else
+            {
+                Scores(false);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (correctButton == 2)
+            {
+                Scores(true);
+            }
+            else
+            {
+                Scores(false);
+            }
+        }
+
+        private void FillArrays()
+        {
+            thaiTable = new string[71, 5] //0=Thai symbol, 1=English Sound, 2= English sound end of syllable, 3= class (low, middle,high), 4=type (vowel, consonant, special)
+            {
+                { "ก", "k", "k", "m", "c" },
+                { "ข", "kh", "k", "h", "c" },
+                { "ค", "kh", "k", "l", "c" },
+                { "ฃ", "kh", "k", "l", "c" },
+                { "ง", "ng", "ng", "l", "c" },
+                { "จ", "c", "t", "m", "c" },
+                { "ฉ", "ch", "t", "h", "c" },
+                { "ช", "ch", "t", "l", "c" },
+                { "ซ", "s", "t", "l", "c" },
+                { "ฌ", "ch", "t", "l", "c" },
+                { "ญ", "y", "n", "l", "c" },
+                { "ฎ", "d", "t", "m", "c" },
+                { "ฏ", "t", "t", "m", "c" },
+                { "ฐ", "th", "t", "h", "c" },
+                { "ฑ", "th", "t", "l", "c" },
+                { "ฒ", "th", "t", "l", "c" },
+                { "ณ", "n", "n", "l", "c" },
+                { "ด", "d", "t", "m", "c" },
+                { "ต", "t", "t", "m", "c" },
+                { "ถ", "th", "t", "h", "c" },
+                { "ท", "th", "t", "l", "c" },
+                { "ธ", "th", "t", "l", "c" },
+                { "น", "n", "n", "l", "c" },
+                { "บ", "b", "p", "m", "c" },
+                { "ป", "p", "p", "m", "c" },
+                { "ผ", "ph", "p", "h", "c" },
+                { "ฝ", "f", "p", "h", "c" },
+                { "พ", "ph", "p", "l", "c" },
+                { "ฟ", "f", "p", "l", "c" },
+                { "ภ", "ph", "p", "l", "c" },
+                { "ม", "m", "m", "l", "c" },
+                { "ย", "y", "y", "l", "c" },
+                { "ร", "r", "r", "l", "c" },
+                { "ล", "l", "n", "l", "c" },
+                { "ว", "w", "w", "l", "c" },
+                { "ศ", "s", "t", "h", "c" },
+                { "ษ", "s", "t", "h", "c" },
+                { "ส", "s", "t", "h", "c" },
+                { "ห", "h", "-", "h", "c" },
+                { "อ", "zero", "-", "m", "s" },
+                { "ออ", "oo", "", "", "v" },
+                { "อะ", "a", "","","v" },
+                { "อั", "a", "","","v" },
+                { "อัว", "au", "","","v" },
+                { "อา", "aa", "","","v" },
+                { "อำ", "am", "","","v" },
+                { "อิ", "i", "","","v" },
+                { "อี", "ii", "","","v" },
+                { "อึ", "u", "","","v" },
+                { "อื", "uu", "","","v" },
+                { "อุ", "u", "","","v" },
+                { "อู", "uu", "","","v" },
+                { "เอ", "ee", "","","v" },
+                { "เอ็", "e", "","","v" },
+                { "เอย", "eey", "","","v" },
+                { "เออ", "ee", "","","v" },
+                { "เออะ", "e", "","","v" },
+                { "เอะ", "e", "","","v" },
+                { "เอา", "aw", "","","v" },
+                { "เอาะ", "c", "","","v" },
+                { "เอิอ", "ee", "","","v" },
+                { "เอีย", "ia", "","","v" },
+                { "เอียะ", "ia", "","","v" },
+                { "เอีอ", "ua", "","","v" },
+                { "แอ", "ee", "","","v" },
+                { "แอ็อ", "e", "","","v" },
+                { "แอะ", "e", "","","v" },
+                { "โอ", "oo", "","","v" },
+                { "โอะ", "o", "","","v" },
+                { "ใอ", "ay", "","","v" },
+                { "ไอ", "ay", "","","v" }
+
+            };
+        }
+
+    }
+}
